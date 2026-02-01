04b8fabb474e3c9645ea65f79ffc6aacdfdde136
LocND2k5 <nguyendinhloc18032005@gmail.com>
2026-02-01 20:46:11 +0700

    Hoan thien chuc nang gio hang

--- src/main/java/com/group2/bookstore/dal/DBContext.java
+++ src/main/java/com/group2/bookstore/dal/DBContext.java
@@ -12,18 +12,18 @@ public class DBContext {
         // OPTION 1: Connection using Named Instance (YOUR ACTIVE SERVER)
         // Use this if your server name is like "DESKTOP-XXX\SQLEXPRESS"
         // =============================================================
-         String serverName = "DESKTOP-V0CJE41\\SQLEXPRESS01"; 
-         String dbName = "BookShop";
-         String userID = "sa";
-         String password = "1";
-         // Connection String for Named Instance (Note: No port number needed here)
-         String url = "jdbc:sqlserver://" + serverName + ";databaseName=" + dbName 
-                    + ";encrypt=true;trustServerCertificate=true;";
+        //  String serverName = "DESKTOP-V0CJE41\\SQLEXPRESS01"; 
+        //  String dbName = "BookShop";
+        //  String userID = "sa";
+        //  String password = "1";
+        //  // Connection String for Named Instance (Note: No port number needed here)
+        //  String url = "jdbc:sqlserver://" + serverName + ";databaseName=" + dbName 
+        //             + ";encrypt=true;trustServerCertificate=true;";
         // =============================================================
         // OPTION 2: Connection using TCP/IP Port (COMMENTED OUT)
         // Use this for standard "localhost" setup on port 1433
         // =============================================================
-        /* String serverName = "localhost";
+         String serverName = "localhost";
         String dbName = "BookShop";
         String portNumber = "1433";
         String instance = ""; // 2022 default instance is empty
@@ -40,7 +40,7 @@ public class DBContext {
             url = "jdbc:sqlserver://" + serverName + "\\" + instance + ":" + portNumber
                     + ";databaseName=" + dbName
                     + ";encrypt=true;trustServerCertificate=true;";
-        } */
+        } 
 
         // 3. Load Driver and Return Connection
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
