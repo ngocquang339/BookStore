6e0826efda95e82149d29137574600b42a5251de
quangakashi <quangakashi@gmail.com>
2026-03-16 13:53:00 +0700

    16/3

--- src/main/java/com/group2/bookstore/dal/BookDAO.java
+++ src/main/java/com/group2/bookstore/dal/BookDAO.java
@@ -333,7 +333,7 @@ public class BookDAO extends DBContext {
                 
                 try { b.setCategoryName(rs.getString("category_name")); } catch (Exception e) {}
                 try { b.setActive(rs.getBoolean("is_active")); } catch (Exception e) {}
-                
+                try { b.setLocationCode(rs.getString("location_code")); } catch (Exception e) {}
                 list.add(b);
             }
         } catch (Exception e) {
